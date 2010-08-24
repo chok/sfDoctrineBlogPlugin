c038c4b88339f9f760e8527c620cf838dad562c0
Maxime Picaud <maxime.picaud@agence-shape.fr>
2010-08-25 01:17:24 +0200

    categories

--- modules/sfBlog/templates/_categories.php
+++ modules/sfBlog/templates/_categories.php
@@ -3,10 +3,12 @@
   <div class="blog-block-content">
     <?php foreach ($categories as $node): ?>
       <?php $category = Doctrine::getTable('Category')->findOneById($node['id']) ?>
-      <div>
-        <?php echo str_repeat('&nbsp;&nbsp;', $node['level']);?>
-        <?php echo link_to($node['name'].'('.$category->getPosts()->count().')', '@blog_category?category='.$node['slug'] ) ?>
-      </div>
+      <?php if($category->getPosts()->count() > 0): //Ultra ultra moche?>
+        <div>
+          <?php echo str_repeat('&nbsp;&nbsp;', $node['level']);?>
+          <?php echo link_to($node['name'].'('.$category->getPosts()->count().')', '@blog_category?category='.$node['slug'] ) ?>
+        </div>
+      <?php endif ?>
     <?php endforeach ?>
   </div>
 </div>
